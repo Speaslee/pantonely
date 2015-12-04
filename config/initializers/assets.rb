4dc130335d7b266128fa42ef6a771279b16bf130
Speaslee <sophiapeaslee@gmail.com>
2015-12-04 10:34:22 -0500

    mad stylings not quite achieved

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,8 +5,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-  config.assets.paths << Rails.root.join("app", "assets", "fonts")
- config.assets.enabled = true
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
