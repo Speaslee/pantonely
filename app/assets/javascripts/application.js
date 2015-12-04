10fdf90d512c5b6ae053b1e8d74e409ed0a4e6a0
Speaslee <sophiapeaslee@gmail.com>
2015-12-04 14:02:47 -0500

    working on autoplay

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,21 +28,3 @@ $(function() {
         "poster": $("#playlist li").eq(0).attr("moviesposter")
     })
 })
-
-video.addEventListener('ended', function () {
-	allLnks[currentVid].classList.remove("currentvid");
-	if ((currentVid + 1) >= lnkNum) {
-		nextVid = 0;
-	} else {
-		nextVid = currentVid+1;
-	}
-	playVid(nextVid);
-})
-
-video.addEventListener('mouseenter',
-	function() {
-		video.setAttribute("controls","true");
-	})
-	video.addEventListener('mouseleave', function() {
-		video.removeAttribute("controls");
-	})
