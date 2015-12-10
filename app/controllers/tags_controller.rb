d8a5228f8384e0e3c5f948ed1118352e63d63a5b
Speaslee <sophiapeaslee@gmail.com>
2015-12-10 13:35:18 -0500

    tabled views

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,5 @@
 class TagsController < ApplicationController
+helper_method :sort_column, :sort_direction
   def index
     @tags = ActsAsTaggableOn::Tag.all
   end
@@ -6,5 +7,14 @@ class TagsController < ApplicationController
   def show
     @tag =  ActsAsTaggableOn::Tag.find(params[:id])
     @songs = Song.where(user_id:current_user.id).tagged_with(@tag.name)
+    @tabled_songs = @songs.order(sort_column + " " + sort_direction)
+  end
+
+  def sort_column
+    Song.column_names.include?(params[:sort])? params[:sort]: "name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction]: "asc"
   end
 end
